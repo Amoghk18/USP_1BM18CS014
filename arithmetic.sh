5869a85f9cd9165461f82c4ec7039b7836a6e8d9
Amoghk18 <53806570+Amoghk18@users.noreply.github.com>
2020-09-24 15:34:15 +0530

    Update arithmetic.sh

--- arithmetic.sh
+++ arithmetic.sh
@@ -3,10 +3,12 @@ read a
 echo "Enter 2nd number"
 read b
 sum=`echo $a + $b|bc`
+sub=`echo $a - $b|bc`
 pro=`echo $a \* $b|bc`
 q=`echo $a / $b|bc`
 mod=`echo $a % $b|bc`
 echo "Sum is "$sum
+echo "Subtraction gives "$sub
 echo "Product is "$pro
 echo "Quotient is "$q
 echo "Remainder is "$mod
