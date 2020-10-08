addc495a3bb8bd5588437282c0eae21c9e3cee15
Amoghk18 <53806570+Amoghk18@users.noreply.github.com>
2020-10-08 02:40:27 -0700

    Add files via upload

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,29 +1,28 @@
 #! /bin/sh
 
-z=0
 echo "The number of arguments are $#"
 if [ $# -eq 2 ]
 then
-if [ -f $1 ]
-then
-echo "Attributes of $1 : "
-ls -l $1
-else
-echo "$1 is not a File"
-fi
-if [ -f $2 ]
-then
-echo "Attributes of $1"
-ls -l $1
-else
-echo "Argument 2 is not a File"
-fi
+  if [ -f $1 ]
+  then
+    echo "Attributes of $1 : "
+    ls -l $1
+  else
+    echo "$1 is not a File"
+    fi
+  if [ -f $2 ]
+  then
+    echo "Attributes of $2"
+    ls -l $2
+  else
+    echo "$2 is not a File"
+    fi
 elif [ $# -gt 2 ]
 then
-echo "Enter only 2 arguments"
+  echo "Enter only 2 arguments"
 elif [ $# -eq 1 ]
 then
-echo "Enter 1 more argument"
+  echo "Enter 1 more argument"
 else
-echo "Enter 2 arguments"
-fi
+  echo "Enter 2 arguments"
+  fi
