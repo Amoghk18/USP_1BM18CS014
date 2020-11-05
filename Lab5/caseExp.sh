231ad4b4280f23509ac7a19999440c5ca8fcd52e
Amoghk18 <53806570+Amoghk18@users.noreply.github.com>
2020-11-05 14:42:03 +0530

    Add files via upload

---
+++ Lab5/caseExp.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+echo "Enter two values on which operations have to be performed : \c"
+read a b
+echo "Operations are : "
+echo "1 -> Add"
+echo "2 -> Subtract" 
+echo "3 -> Multiply" 
+echo "4 -> Div" 
+echo "5 -> Mod" 
+echo "Enter choice" 
+read op 
+case $op in 
+        1)echo Addition: $(expr $a + $b)
+        ;; 
+        2)echo Subtraction: $(expr $a - $b)
+        ;; 
+        3)echo Multiplication: $(expr $a \* $b)
+        ;; 
+        4)echo Division: $(expr $a / $b)
+        ;; 
+        5)echo Modulus: $(expr $a % $b)
+        ;; 
+        *)echo Not a valid choice
+esac
